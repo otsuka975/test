c4d6557e2c2c3f8203e1746bd3af4124bf2791e2
asct05 <asct05@T-PC05.mshome.net>
2023-09-05 15:36:10 +0900

    testtest

---
+++ src/test/Dice.java
@@ -0,0 +1,17 @@
+package test;
+
+import java.util.Random;
+
+public class Dice {
+    private Random r;
+    private int n;
+
+    public Dice(int n) {
+        r = new Random();
+        this.n = n;
+    }
+
+    public int cast() {
+        return r.nextInt(n) + 1;
+    }
+}
